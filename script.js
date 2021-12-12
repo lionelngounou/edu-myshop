1528b4696acdc7726fab6f912eab9baddde61ea9
Kevin Njah <kcnjah@gmail.com>
2021-12-12 10:35:11 +0000

    Updated table (Delete row not working)

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ const PRODUCTS_LIST = [
     {
         name: "Milk",
         quantity: 15,
-        price: 1.20
+        price: '£'+1.2+'0'
     },
     {
         name: "Candy",
         quantity: 9,
-        price: 0.35
+        price: '£'+0.35
     },
     {
         name: "Chocolate",
         quantity: 20,
-        price: 0.60
+        price: '£'+0.6+'0'
     }
 ];
 
@@ -44,13 +44,13 @@ function addProductsToTable(tableElement){
         const newRow = tableElement.insertRow(newRowIndex); 
         newRow.insertCell(0).innerHTML = product.name; //column 1
         newRow.insertCell(1).innerHTML = product.quantity; //column 2
-        newRow.insertCell(2).innerHTML = "&#163; " + product.price; //column 3
+        newRow.insertCell(2).innerHTML = product.price; //column 3
         const deleteButtonHTML = `<button class="deleteBtn btn" 
-            onclick="removeProduct(${index}); displayProducts();" 
+            onclick="removeProductFromList(${index});removeAllRowsOfProduct(${index}); displayProducts();" 
             >Delete</button>`;  
         newRow.insertCell(3).innerHTML = deleteButtonHTML; //column 4
     }
-}
+}/**/
 
 function removeAllRowsOfProduct(tableElement){
     let numberOfRows = tableElement.rows.length;
@@ -61,22 +61,42 @@ function removeAllRowsOfProduct(tableElement){
 }
 
 function saveOrUpdateProduct(event){
-    event.preventDefault();
-    //get details from the form
-    const formName = document.getElementById("name").value;
-    const formQuantity = document.getElementById("quantity").value;
-    const formPrice = document.getElementById("price").value;
-    //set new product object
-    const newProduct = {
-            name: formName,
-            quantity: formQuantity,
-            price: formPrice
-        };
-    console.log(newProduct);
-    
-    // add product and refresh display 
-    addProductToList(newProduct);
-    displayProducts();
+    var itemName = document.getElementById("name").value;
+    var quantity = document.getElementById("quantity").value;
+    var price = document.getElementById("price").value;
+
+    if (itemName === "") {
+        document.getElementById("popup").style.display="block";
+        document.getElementById("name").style.borderBottomColor="red";
+    }
+    else if (quantity === "") {
+        document.getElementById("popupQuantity").style.display="block";
+        document.getElementById("quantity").style.borderBottomColor="red";
+    }
+    else if (price === "") {
+        document.getElementById("popupPrice").style.display="block";
+        document.getElementById("price").style.borderBottomColor="red";
+    }
+    else{
+        event.preventDefault();
+        //get details from the form
+        const formName = document.getElementById("name").value;
+        const formQuantity = document.getElementById("quantity").value;
+        const formPrice = document.getElementById("price").value;
+        //set new product object
+        const newProduct = {
+                name: formName,
+                quantity: formQuantity,
+                price: formPrice
+            };
+        console.log(newProduct);
+         //add product and refresh display 
+        addProductToList(newProduct);
+        displayProducts();
+        return true;
+    }
+clearForm();
+console.log(PRODUCTS_LIST);
 }
 
 function clearForm(){
@@ -84,26 +104,3 @@ function clearForm(){
     document.getElementById("quantity").value = "";
     document.getElementById("price").value = "";
 }
-
-function checkEmptyInput() {
-    var isEmpty = false;
-    var itemName = document.getElementById("name").value;
-    var quantity = document.getElementById("quantity").value;
-    var price = document.getElementById("price").value;
-
-    if (itemName === "") {
-        alert("Item Name cannot Be Empty");
-        isEmpty = true;
-    }
-    else if (quantity === "") {
-        alert("Quantity cannot Be Empty");
-        isEmpty = true;
-    }
-    else if (price === "") {
-        alert("Price cannot Be Empty");
-        isEmpty = true;
-    }
-    return isEmpty;
-}          
- 
-displayProducts();
